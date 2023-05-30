f728742ebe0b9634c3b6a413a97c42d5865cccc4
Kyle <kylekibbe99@gmail.com>
2023-05-29 21:46:38 -0700

    Creating and inserting dom node elements

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.textContent = "Kyle is the champion";
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
